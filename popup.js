19fb660986ae61ff967946d5ebbea6d802c8fcb6
Jack Bailey <jack@jackbailey.dev>
2023-04-18 22:10:29 +0200

    Downgrade to Manifest v2 for Firefox Support (#1)
    * Make start on V3 => V2 downgrade
    
    * Downgrade to Manifest V2

--- popup.js
+++ popup.js
@@ -65,7 +65,6 @@ const loadPage = (newPage) => {
     switch (newPage) {
         case "new-link":
             chrome.tabs.query({active: true}, tabs => {
-                console.log(tabs)
                 if (tabs[0]) {
                     currentURL = tabs[0].url;
 
@@ -104,11 +103,19 @@ const setError = (error, text) => {
 }
 
 const saveValues = () => {
-    chrome.storage.local.set(settings);
+    chrome.storage.sync.set(settings);
 }
 
-const getValues = async () => {
-    settings = await chrome.storage.local.get(["domain", "secret"]);
+const getValues = () => {
+    return new Promise ((resolve, reject) => {
+        chrome.storage.sync.get(["domain", "secret"], (data) => {
+            resolve(data)
+        })
+    })
+}
+
+const loadValues = async () => {
+    settings = await getValues()
     const settingsPage = document.getElementById("settings-page");
 
     [...settingsPage.querySelectorAll("input")].forEach((setting) => {
@@ -315,11 +322,7 @@ const init = async () => {
 
     let dark;
 
-    if (localStorage.getItem("dark") !== null) {
-        dark = localStorage.getItem("dark") === "true";
-    } else {
-        dark = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) || false;
-    }
+    dark = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) || false;
 
     if (dark) document.documentElement.classList.add("dark");
     else document.documentElement.classList.remove("dark");
@@ -329,7 +332,6 @@ const init = async () => {
     const toggleSecret = document.getElementById("toggle-secret");
     toggleSecret.addEventListener("click", () => {
         const secretInput = document.getElementById("secret-input");
-        console.log(toggleSecret.dataset)
         toggleSecret.setAttribute("data-secretvisible", !(toggleSecret.dataset.secretvisible === "true"));
         secretInput.type = toggleSecret.dataset.secretvisible === "true" ? "text" : "password";
     })
@@ -337,14 +339,13 @@ const init = async () => {
     // New link
 
     const newLinkButton = document.getElementById("new-link-button");
-    console.log(newLinkButton)
     newLinkButton.addEventListener("click", () => {
         let destination = document.getElementById("new-link-destination").value;
         let slug = document.getElementById("new-link-slug").value;
         createLink(destination, slug);
     })
 
-    await getValues();
+    await loadValues();
     loadPage("links");
 }
 
